e3d218ffed39c0b4f4c1ca952d3b9ce40d77b8d7
Chris Oakman <chris@oakmac.com>
2022-09-17 17:41:28 -0500

    initial commit: HumbleUI Mondrian Painting

---
+++ README.md
@@ -0,0 +1,27 @@
+# Mondrian Painting in HumbleUI
+
+As a challenge for my coding bootcamp students when teaching them CSS, I would
+ask them to build this [Piet Mondrian painting] using flexbox.
+
+As an exercise for myself (and to test out the layout system), I did the same
+using [HumbleUI].
+
+This project originally adapted from [humble-deck].
+
+[Piet Mondrian painting]:https://github.com/oakmac/flexbox-layout-exercises#problem-3---piet-mondrian-painting
+[HumbleUI]:https://github.com/HumbleUI/HumbleUI
+[humble-deck]:https://github.com/tonsky/humble-deck
+
+## Development
+
+```sh
+## run the program
+./script/run.sh
+
+## Run a REPL
+clj -M:repl/rebel
+```
+
+## License
+
+[ISC License](LICENSE.md)
---
+++ src/humble_mondrian/main.clj
@@ -0,0 +1,220 @@
+(ns humble-mondrian.main
+  (:require
+    [io.github.humbleui.paint :as paint]
+    [io.github.humbleui.ui :as ui]
+    [io.github.humbleui.window :as window]))
+
+(set! *warn-on-reflection* true)
+
+(def dark-grey 0xff404040)
+(def blue 0xff0d7fbe)
+(def yellow 0xfffae317)
+(def red 0xfff50f0f)
+(def white 0xfff3f3f3)
+(def black 0xff000000)
+
+(def outside-padding 15)
+(def inner-padding 10)
+
+(defonce *window
+  (atom nil))
+
+(defn redraw []
+  (some-> @*window window/request-frame))
+
+;; TODO: is there an empty component I can use instead of label = "" ?
+(defn Box [color]
+  (ui/rect (paint/fill color) (ui/label "")))
+
+(def LeftColumn
+  (ui/column
+    [:stretch 1 (ui/row
+                  [:stretch 130 (Box white)]
+                  (ui/gap inner-padding 0)
+                  [:stretch 220 (Box white)])]
+    (ui/gap 0 inner-padding)
+    [:stretch 8 (ui/row
+                  [:stretch 9 (ui/column
+                                [:stretch 1 (Box white)]
+                                (ui/gap 0 inner-padding)
+                                [:stretch 1 (Box white)]
+                                (ui/gap 0 inner-padding)
+                                [:stretch 1 (Box yellow)])]
+                  (ui/gap inner-padding 0)
+                  [:stretch 27 (ui/column
+                                 [:stretch 1 (Box red)]
+                                 (ui/gap 0 inner-padding)
+                                 [:stretch 1 (ui/row
+                                              [:stretch 1
+                                                (ui/column
+                                                  [:stretch 1 (Box black)]
+                                                  (ui/gap 0 inner-padding)
+                                                  [:stretch 1 (Box white)])]
+                                              (ui/gap inner-padding 0)
+                                              [:stretch 1
+                                                (ui/column
+                                                  [:stretch 1 (Box white)]
+                                                  (ui/gap 0 inner-padding)
+                                                  [:stretch 1 (Box white)]
+                                                  (ui/gap 0 inner-padding)
+                                                  [:stretch 1 (Box black)]
+                                                  (ui/gap 0 inner-padding)
+                                                  [:stretch 1
+                                                    (ui/row
+                                                      [:stretch 1 (Box yellow)]
+                                                      (ui/gap inner-padding 0)
+                                                      [:stretch 1 (Box white)])])])])])]
+    (ui/row
+      [:stretch 9 (Box dark-grey)]
+      (ui/gap inner-padding 0)
+      [:stretch 27 (ui/row
+                      [:stretch 1 (Box dark-grey)]
+                      (ui/gap inner-padding 0)
+                      [:stretch 1 (ui/row
+                                    [:stretch 1 (Box yellow)]
+                                    (ui/gap inner-padding 0)
+                                    [:stretch 1 (Box white)])])])
+    [:stretch 1
+     (ui/row
+      [:stretch 9 (Box white)]
+      (ui/rect (paint/fill white)
+        (ui/gap inner-padding 0))
+      [:stretch 27 (ui/row
+                      [:stretch 1 (Box white)]
+                      (ui/gap inner-padding 0)
+                      [:stretch 1
+                       (ui/row
+                        [:stretch 1 (Box yellow)]
+                        (ui/gap inner-padding 0)
+                        [:stretch 1 (Box white)])])])]))
+
+(def CenterTopLeft
+  (ui/column
+    [:stretch 1 (Box yellow)]
+    (ui/gap 0 inner-padding)
+    [:stretch 2 (Box yellow)]
+    (ui/gap 0 inner-padding)
+    [:stretch 2 (ui/row
+                  [:stretch 1 (Box white)]
+                  (ui/gap inner-padding 0)
+                  [:stretch 1 (Box white)])]))
+
+(def CenterTopRight
+  (ui/column
+    [:stretch 2 (Box blue)]
+    (ui/gap 0 inner-padding)
+    [:stretch 2 (Box red)]
+    (ui/gap 0 inner-padding)
+    [:stretch 1 (Box white)]))
+
+(def CenterBottomLeft
+  (ui/column
+    [:stretch 1 (Box white)]
+    (ui/gap 0 inner-padding)
+    [:stretch 2 (Box blue)]
+    (ui/gap 0 inner-padding)
+    [:stretch 2 (Box red)]))
+
+(def CenterBottomRight
+  (ui/column
+    [:stretch 2 (ui/row
+                  [:stretch 1 (Box white)]
+                  (ui/gap inner-padding 0)
+                  [:stretch 1 (Box white)])]
+    (ui/gap 0 inner-padding)
+    [:stretch 2 (Box yellow)]
+    (ui/gap 0 inner-padding)
+    [:stretch 1 (ui/row
+                  [:stretch 1 (Box white)]
+                  (ui/gap inner-padding 0)
+                  [:stretch 1 (Box black)])]))
+
+(def CenterTop
+  (ui/row
+    [:stretch 1 CenterTopLeft]
+    (ui/gap inner-padding 0)
+    [:stretch 1 CenterTopRight]))
+
+(def CenterBottom
+  (ui/row
+    [:stretch 1 CenterBottomLeft]
+    (ui/gap inner-padding 0)
+    [:stretch 1 CenterBottomRight]))
+
+(def CenterColumn
+  (ui/column
+    [:stretch 1 CenterTop]
+    (ui/gap 0 inner-padding)
+    [:stretch 1 CenterBottom]))
+
+(def RightColumnTop
+  (ui/row
+    [:stretch 1 (Box yellow)]
+    (ui/gap inner-padding 0)
+    [:stretch 1 (Box black)]
+    (ui/gap inner-padding 0)
+    [:stretch 4 (Box white)]))
+
+(def RightColumnCenter
+  (ui/row
+    [:stretch 4
+      (ui/column
+        [:stretch 1 (ui/row
+                      [:stretch 1 (ui/column
+                                    [:stretch 1 (Box yellow)]
+                                    (ui/gap 0 inner-padding)
+                                    [:stretch 1 (Box black)]
+                                    (ui/gap 0 inner-padding)
+                                    [:stretch 1 (Box white)]
+                                    (ui/gap 0 inner-padding)
+                                    [:stretch 1 (Box white)])]
+                      (ui/gap inner-padding 0)
+                      [:stretch 1 (ui/column
+                                    [:stretch 1 (Box white)]
+                                    (ui/gap 0 inner-padding)
+                                    [:stretch 1 (Box black)])])]
+        (ui/gap 0 inner-padding)
+        [:stretch 1 (Box blue)])]
+    (ui/gap inner-padding 0)
+    [:stretch 1
+      (ui/column
+        [:stretch 3 (Box yellow)]
+        (ui/gap 0 inner-padding)
+        [:stretch 3 (Box white)]
+        (ui/gap 0 inner-padding)
+        [:stretch 2 (Box white)])]))
+
+(def RightColumnBottom
+  (ui/row
+    [:stretch 3 (Box white)]
+    (ui/gap inner-padding 0)
+    [:stretch 2 (Box white)]))
+
+(def RightColumn
+  (ui/column
+    [:stretch 1 RightColumnTop]
+    (ui/gap 0 inner-padding)
+    [:stretch 8 RightColumnCenter]
+    (ui/gap 0 inner-padding)
+    [:stretch 1 RightColumnBottom]))
+
+(def MondrianApp
+  (ui/default-theme {}
+    (ui/rect (paint/fill dark-grey)
+      (ui/padding outside-padding
+        (ui/row
+          [:stretch 1 LeftColumn]
+          (ui/gap inner-padding 0)
+          [:stretch 1 CenterColumn]
+          (ui/gap inner-padding 0)
+          [:stretch 1 RightColumn])))))
+
+;; re-draw the UI when we load this namespace
+(redraw)
+
+(defn -main [& args]
+  (reset! *window
+    (ui/start-app!
+      {:title    "HumbleUI Mondrian"
+       :bg-color 0xFFFFFFFF}
+      #'MondrianApp)))
